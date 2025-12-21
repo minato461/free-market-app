26cc45b8c0292054278a527e44a6ffe30ebeefb1
スーハイ <sanku461@gmail.com>
2025-12-21 10:13:25 +0900

    プロフィール画面について

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>マイページ | フリマアプリ</title>
+    <title>プロフィール | フリマアプリ</title>
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
